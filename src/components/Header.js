c6af0b3482f7d1112bedb0a9a3b9a9fe294df801
“Ali338991” <ali.ansari7268@gmail.com>
2022-05-26 20:41:32 +0500

    expense tracker code Update

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div style={{textAlign:'center'}}>
+      <h2 style={{ color: "red" }}>Developed By Ali Abubakar</h2>{" "}
+      <h2>Expense Tracker</h2>
+    </div>
+  );
+};
